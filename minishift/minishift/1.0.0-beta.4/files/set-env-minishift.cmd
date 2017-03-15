3aa45818938b57a22b5a45b7a846138c1037490b
Simon Jahreiß <simon.jahreiss@qaware.de>
2017-03-15 18:19:03 +0100

    Added comments to set-env-minishift.cmd file.

--- minishift/minishift/1.0.0-beta.4/files/set-env-minishift.cmd
+++ minishift/minishift/1.0.0-beta.4/files/set-env-minishift.cmd
@@ -1,8 +1,13 @@
 @echo off
 
+:: Add Minishift and the OpenShift CLI to the path
 set PATH=%SOFTWARE_DIR%\minishift-1.0.0-beta.4;%PATH%
 set PATH=%HOME%\.minishift\cache\oc\v1.4.1;%PATH%
+
+:: Correct the home variables which are used by Minishift
 set USERPROFILE=%HOME%
 set HOMEPATH=%HOME%
 set HOMEDRIVE=%SEU_HOME%
+
+:: Use VirtualBox for virtualization
 set MINISHIFT_VM_DRIVER=virtualbox
