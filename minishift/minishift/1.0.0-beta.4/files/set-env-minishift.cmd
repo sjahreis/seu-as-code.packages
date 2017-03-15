4535c381af325bbd1463618d0d98d99f522a51c6
Simon Jahreiß <simon.jahreiss@qaware.de>
2017-03-15 18:06:33 +0100

    Added first version of a minishift package.

---
+++ minishift/minishift/1.0.0-beta.4/files/set-env-minishift.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set PATH=%SOFTWARE_DIR%\minishift-1.0.0-beta.4;%PATH%
+set PATH=%HOME%\.minishift\cache\oc\v1.4.1;%PATH%
+set USERPROFILE=%HOME%
+set HOMEPATH=%HOME%
+set HOMEDRIVE=%HOME%
+set MINISHIFT_VM_DRIVER=virtualbox
