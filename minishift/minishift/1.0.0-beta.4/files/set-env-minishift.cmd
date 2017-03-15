8822ea00b0dd92631d23c8d0d3ac7f6543239f9d
Simon Jahreiß <simon.jahreiss@qaware.de>
2017-03-15 18:17:03 +0100

    Corrected HOMEDRIVE environment variable.

--- minishift/minishift/1.0.0-beta.4/files/set-env-minishift.cmd
+++ minishift/minishift/1.0.0-beta.4/files/set-env-minishift.cmd
@@ -4,5 +4,5 @@ set PATH=%SOFTWARE_DIR%\minishift-1.0.0-beta.4;%PATH%
 set PATH=%HOME%\.minishift\cache\oc\v1.4.1;%PATH%
 set USERPROFILE=%HOME%
 set HOMEPATH=%HOME%
-set HOMEDRIVE=%HOME%
+set HOMEDRIVE=%SEU_HOME%
 set MINISHIFT_VM_DRIVER=virtualbox
